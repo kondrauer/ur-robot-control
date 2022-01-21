e3cbffda184af4338565fb682f691487db9758b5
Kondrauer <maximilian.schlosser@yahoo.de>
2022-01-21 13:02:50 +0100

    WOCHENENDE SAUFEN

--- PolicyGUI.py
+++ PolicyGUI.py
@@ -6,34 +6,73 @@ Created on Fri Jan 21 10:18:41 2022
 """
 
 import tkinter as tk
+import numpy as np
+
+from PIL import ImageTk, Image
+
+from RealsenseInterface import RealsenseInterface
 
 class Toolbar(tk.Frame):
 	
 	def __init__(self, parent, *args, **kwargs):
-			tk.Frame.__init__(self, parent, *args, **kwargs)
-			self.parent = parent
-			
-			test = tk.Label(self, text="Toolbar")
-			test.pack()
+		tk.Frame.__init__(self, parent, *args, **kwargs)
+		self.parent = parent
+		
+		menubar = tk.Menu(self.parent.parent)
+		self.parent.parent.config(menu=menubar)
+		
+		fileMenu = tk.Menu(menubar, tearoff=0)
+		fileMenu.add_command(label="Exit", command=self.parent.onExit)
+		menubar.add_cascade(label="File", menu=fileMenu)
+		
+
 
 class VideoFeed(tk.Frame):
 	
 	def __init__(self, parent, *args, **kwargs):
-			tk.Frame.__init__(self, parent, *args, **kwargs)
-			self.parent = parent
+		tk.Frame.__init__(self, parent, *args, **kwargs)
+		self.parent = parent
+		
+		self.realsense = RealsenseInterface(align=True)
+		self.realsense.start()
+		
+		self.videoStream = tk.Label(self)
+		self.videoStream.pack(padx=5, pady=5)
+		
+		self.getNewFrames(self.realsense)
 			
-			test = tk.Label(self, text="VideoFeed")
-			test.pack()
+	def getNewFrames(self, rs: RealsenseInterface):
+		
+		rs.getFrames()
+		
+		colorImage = np.array(rs.colorFrame.get_data())
+		
+		#convert from bgr to rgb
+		colorImage = colorImage[:,:,::-1]
+		
+		img = Image.fromarray(colorImage)
+		img = img.resize((320, 240), Image.ANTIALIAS)
+		imgtk = ImageTk.PhotoImage(image=img)
+		self.videoStream.imgtk = imgtk
+		self.videoStream.configure(image=imgtk)
+		self.afterID = self.after(1, lambda: self.getNewFrames(self.realsense))
+		
 			
-class GraspViewer(tk.Frame):
+class GraspViewer(tk.LabelFrame):
 	
 	def __init__(self, parent, *args, **kwargs):
-			tk.Frame.__init__(self, parent, *args, **kwargs)
+			tk.LabelFrame.__init__(self, parent, *args, **kwargs)
 			self.parent = parent
 			
-			test = tk.Label(self, text="GraspViewer")
-			test.pack()
-
+			self.graspCanvas = tk.Canvas(self)
+			self.informationLabel
+			self.graspCanvas.pack(fill="both")
+			
+			self.img = Image.open(r'img\color_0.png')
+			self.img = self.img.resize((320, 240), Image.ANTIALIAS)
+			self.img = ImageTk.PhotoImage(self.img)
+			self.graspCanvas.create_image((0, 0), anchor=tk.NW, image=self.img)
+			
 class Actions(tk.Frame):
 	
 	def __init__(self, parent, *args, **kwargs):
@@ -68,10 +107,10 @@ class UpperFrame(tk.Frame):
 			self.parent = parent
 			
 			self.videoFeed = VideoFeed(self)
-			self.graspViewer = GraspViewer(self)
+			self.graspViewer = GraspViewer(self, text="Predicted Grasp")
 			
-			self.videoFeed.pack(side = "left")
-			self.graspViewer.pack(side = "right")
+			self.videoFeed.pack(side = "left", fill=tk.X)
+			self.graspViewer.pack(side = "right", fill=tk.X)
 	
 class LowerFrame(tk.Frame):
 	
@@ -82,8 +121,8 @@ class LowerFrame(tk.Frame):
 			self.actions = Actions(self)
 			self.depthSegmentationViewer = DepthSegmentationViewer(self)
 			
-			self.actions.pack(side = "left")
-			self.depthSegmentationViewer.pack(side = "right")
+			self.actions.pack(side="left")
+			self.depthSegmentationViewer.pack(side="right")
 
 class PolicyApp(tk.Frame):
 	
@@ -100,13 +139,19 @@ class PolicyApp(tk.Frame):
 			
 			
 			self.toolbar.pack(side = "top")
-			self.upperFrame.pack(side = "top")
+			self.upperFrame.pack(side = "top", fill=tk.X, padx=5)
 			self.debugLogger.pack(side = "bottom")
-			self.lowerFrame.pack(side = "bottom")
+			self.lowerFrame.pack(side = "bottom", fill=tk.X)
 			
+		def onExit(self):
+			
+			self.upperFrame.videoFeed.after_cancel(self.upperFrame.videoFeed.afterID)
+			self.parent.destroy()
 			
 			
 if __name__ == "__main__":
 	root = tk.Tk()
-	PolicyApp(root).pack(side="top", fill="both", expand=True)
+	pa = PolicyApp(root)
+	pa.pack(side="top", fill="both", expand=True)
+	root.protocol("WM_DELETE_WINDOW", pa.onExit)
 	root.mainloop()
